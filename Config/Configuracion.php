12e2fb0c15beeb9a77885d1c30377bef029fb38b
Joshe Oñate <joshe.onate@umayor.cl>
2016-03-31 13:02:59 -0300

    Se cambia configuracion local

--- Config/Configuracion.php
+++ Config/Configuracion.php
@@ -14,9 +14,9 @@
 class Configuracion {
     
     public static $DB_NAME = 'FAM';
-    public static $DB_USER = 'valentys_sql';
-    public static $DB_PASS = "valentys.2012*";
-    public static $DB_SERVER = "172.16.39.64";
+    public static $DB_USER = 'sa';
+    public static $DB_PASS = "informatica.2014*";
+    public static $DB_SERVER = "localhost";
     
     function getDB_NAME() {
         return $this->DB_NAME;
